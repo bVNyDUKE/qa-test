7ff043dffe5b87b7f391039f6c2746d9d82312b2
Darko Ivkov <bVNyDUKE@protonmail.com>
2023-01-05 19:14:47 +0100

    add redirect to posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import { helloWrapper } from '@/utils/helloWrapper';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import { PostsProvider } from '@/store';
 import PostsListPage from '@/pages/PostListPage';
 import PostPage from '@/pages/PostPage';
-import HomePage from './pages/HomePage';
 
-function App() {
+const App = () => {
   return (
     <Layout>
       <PostsProvider>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Navigate to="/posts" />} />
           <Route path="/posts" element={<PostsListPage />} />
           <Route path="/posts/:postId" element={<PostPage />} />
         </Routes>
       </PostsProvider>
     </Layout>
   );
-}
+};
 
 export default helloWrapper(App);
